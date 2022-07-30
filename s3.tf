181d1d07a169880590180c4ecc46cb7844fe23e4
Raymond Liu <rayliuy25@gmail.com>
2022-07-29 21:43:38 -0400

    update

--- s3.tf
+++ s3.tf
@@ -3,3 +3,9 @@ module "s3" {
   common_tags = local.common_tags
   bucket_name = local.s3_bucket_name
 }
+
+resource "aws_s3_object" "object" {
+  bucket = module.s3.bucket_id
+  key    = "/website/index.html"
+  source = "./website/index.html"
+}
